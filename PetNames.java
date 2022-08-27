5967579b46d97ea465addbb6ff432c00f05f6ca0
Dzheb <Dzheb2009@gmail.com>
2022-08-27 14:23:55 +0300

    Listof finish

--- PetNames.java
+++ PetNames.java
@@ -1,19 +1,18 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+
 public class PetNames {
   private PetNames() {
   }
 
   public static ArrayList<String> getCatNames() {
-    ArrayList<String> catNames = new ArrayList<>(Arrays.asList("Барсик","Пират","Пушок","Васька"));
+    ArrayList<String> catNames = new ArrayList<>(Arrays.asList("Барсик", "Пират", "Пушок", "Васька"));
     return catNames;
   }
 
   public static ArrayList<String> getDogNames() {
-    // ArrayList<String> dogNames = new ArrayList<>();
     ArrayList<String> dogNames = new ArrayList<>(Arrays.asList("Барбос",
-    "Бобик","Шарик","Мухтар"));
+        "Бобик", "Шарик", "Мухтар"));
     return dogNames;
   }
 
